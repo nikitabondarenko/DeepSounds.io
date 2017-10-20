5a864d1cd0ceefdd9781fbcbde5d46d0c4e69215
bvi1994 <bvi1@ucsc.edu>
2017-10-20 15:06:08 -0700

    Completed lyrics converstion and sentiment analysis on the song. Currently, we are returning the lyrics as an array with each element being a line. The sentiment analysis is an array of objects

--- analyzelyrics.py
+++ analyzelyrics.py
@@ -1,7 +1,13 @@
+import sentiment
 
-lyrArr = []
-with open('lyrics.txt','r', encoding='utf-8') as f:
-    for line in f:
-        for word in line.split():
-            if ('[' not in word and ']' not in word and 'Verse' not in word):
-                lyrArr.append(word)     
+def analyze():
+    lyrArr = []
+    with open('lyrics.txt','r', encoding='utf-8') as f:
+        for line in f:
+            for word in line.split('\n'):
+                if ('[' not in word and ']' not in word and 'Verse' not in word and word != ''):
+                    lyrArr.append(word)
+        # print(lyrArr)
+        # print(sentiment.string(lyrArr))
+        return (lyrArr, sentiment.rate(lyrArr))
+        # We are returning a tuple, the lyrics array and the lyrics array rating.
--- sentiment.py
+++ sentiment.py
@@ -3,4 +3,14 @@ import os
 
 indicoio.config.api_key = os.environ["INDICOIO_API_KEY"]
 
-print(indicoio.emotion("I do not like green eggs and ham!")
+def rate(lyrics):
+    if((type(lyrics) is str)):
+        lyrics = lyrics.split('\n')
+    # print(lyrics)
+    lyricsEmotion = []
+    for line in lyrics:
+        lyricsEmotion.append(indicoio.emotion(line))
+    return lyricsEmotion
+
+
+# print(string("I do not like this. \nBecause I said so."))
