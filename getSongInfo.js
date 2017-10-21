b92cdd2b8a8b69c5f79907fe7b4fc012c527acbe
bvi1994 <bvi1@ucsc.edu>
2017-10-21 01:15:17 -0700

    Added barGraph features, renamed some files

--- getSongInfo.js
+++ getSongInfo.js
@@ -1,7 +1,7 @@
 var plotly = require('plotly')(process.env.PLOTLY_USERNAME, process.env.PLOTLY_API_KEY);
 
 module.exports = {
-  printObj: function(body){
+  timeSeries: function(body){
     var lineNumber = [];
     for(var i = 1; i <= body.lyricsLine.length; i++){
       lineNumber.push(i);
@@ -59,14 +59,55 @@ module.exports = {
       text: body.lyricsLine,
       line: {color: 'cyan'},
     }
-    var layout = {fileopt : "overwrite", filename : "test"};
+    var layout = {fileopt : "overwrite",
+                  filename : "test",
+                  // xaxis: {title: 'Lyrics Line Number'},
+                  // yaxis: {title: 'Intensity'}
+                };
     var data = [anger, joy, fear, surprise, sadness];
 
     plotly.plot(data, layout, function (err, msg) {
     	if (err) return console.log(err);
     	console.log(msg);
     });
+    return;
+  }
+}
 
+module.exports = {
+  barGraph: function (body){
+    var angerArray = [];
+    var joyArray = [];
+    var fearArray = [];
+    var surpriseArray = [];
+    var sadnessArray = []
+    for(var i = 0; i < body.lineRating.length; i++){
+      angerArray.push(body.lineRating[i].anger);
+      joyArray.push(body.lineRating[i].joy);
+      fearArray.push(body.lineRating[i].fear);
+      surpriseArray.push(body.lineRating[i].surprise);
+      sadnessArray.push(body.lineRating[i].sadness);
+    }
+    var angerAverage = angerArray.reduce((a, b) => (a + b), 0) / body.lineRating.length;
+    var joyAverage = joyArray.reduce((a, b) => (a + b), 0) / body.lineRating.length;
+    var fearAverage = fearArray.reduce((a, b) => (a + b), 0) / body.lineRating.length;
+    var surpriseAverage = surpriseArray.reduce((a, b) => (a + b), 0) / body.lineRating.length;
+    var sadnessAverage = sadnessArray.reduce((a, b) => (a + b), 0) / body.lineRating.length;
+    // console.log(angerAverage);
+    var data = [{
+      x: ["anger", "joy", "fear", "surprise", "sadness"],
+      y: [angerAverage, joyAverage, fearAverage, surpriseAverage, sadnessAverage],
+      marker: {color: ['red', 'green', 'purple', 'orange', 'cyan']},
+      type: "bar"
+    }];
+    var layout = {filename: 'test2',
+                  fileopt: 'overwrite',
+                  // xaxis: {title: 'Mood'},
+                  // yaxis: {title: 'Average Intensity'}
+                };
+    plotly.plot(data, layout, function (err, msg) {
+      console.log(msg);
+    });
   }
 }
 
--- processInfo.js
+++ processInfo.js
@@ -1,11 +1,13 @@
-var printObjects = require('./getSongInfo.js')
+var graph = require('./getSongInfo.js')
 var request = require('request');
 
 // console.log("Hi");
 // console.log(printObjects)
 function getLyricsData(){
   request('https://arcane-oasis-93825.herokuapp.com/lyrics', function (error, response, body) {
-    printObjects.printObj(JSON.parse(body));
+    body = JSON.parse(body);
+    // graph.timeSeries(body);
+    graph.barGraph(body);
     // return body;
     return;
   });
